e5e0f69657497bd40602b34dd52f03f278623c8d
msfnunesdev-commits <msfnunes.dev@gmail.com>
2026-05-17 22:56:13 -0300

    Move exercício login simples para estrutura de condicionais

--- login.py
+++
@@ -1,9 +0,0 @@
-print("olá, seja bem vindo!")
-
-usuario = input("Login: ")
-senha = input("senha: ")
-
-if usuario=="admin" and senha=="1234":
-    print("Acesso permitido")
-else:
-    print("Acesso negado")
