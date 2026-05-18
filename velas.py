9c3aaf52e9190c14bd92734dd9ea13ffbbc1b545
msfnunesdev-commits <msfnunes.dev@gmail.com>
2026-05-17 22:57:47 -0300

    Move exercício velas para cálculos básicos

--- velas.py
+++
@@ -1,8 +0,0 @@
-print("Olá, seja bem vindo!")
-
-ano_atual = int(input("Em que ano nós estamos? "))
-ano_nasc = int(input("Em que ano você nasceu? "))
-
-idade = ano_atual - ano_nasc
-
-print("A quantidade de velas no bolo é ",idade)
