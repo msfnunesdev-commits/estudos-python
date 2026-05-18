c6309b581147f4b392dc4de1b9f9235aa9023ce5
msfnunesdev-commits <msfnunes.dev@gmail.com>
2026-05-17 22:56:37 -0300

    Move exercício positivo, negativo ou zero para estrutura de condicionais

--- positivo_negativo_zero.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Olá, me informe um número: "))
-
-if x>0:
-    print("O número ", x, "é maior que zero.")
-elif x<0:
-    print("O número ", x, "é menor que zero.")
-else:
-    print("O número ", x, "é igual a zero.")
