df3e54aa984db05dd17fa79d62ddd578aa641f64
msfnunesdev-commits <msfnunes.dev@gmail.com>
2026-05-17 22:53:49 -0300

    Organiza exercício condicional: maior de idade

---
+++ 01_fundamentos/condicionais/maior_de_idade.py
@@ -0,0 +1,7 @@
+N1 = str(input("Olá, digite o seu nome: "))
+N2 = int(input("Informe a sua idade: "))
+
+if N2 >= 18:
+    print(N1, ",você é maior de idade!")
+else:
+    print(N1, ",você é menor de idade!")
