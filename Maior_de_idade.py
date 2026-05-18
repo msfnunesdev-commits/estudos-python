607afe4a0c7c0d2754e72e6aec78715d0aa6c45a
msfnunesdev-commits <msfnunes.dev@gmail.com>
2026-05-17 22:56:03 -0300

    Move exercício maior de idade para estrutura de condicionais

--- Maior_de_idade.py
+++
@@ -1,7 +0,0 @@
-N1 = str(input("Olá, digite o seu nome: "))
-N2 = int(input("Informe a sua idade: "))
-
-if N2 >= 18:
-    print(N1, ",você é maior de idade!")
-else:
-    print(N1, ",você é menor de idade!")
