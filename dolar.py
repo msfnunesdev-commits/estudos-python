b1be71a7a2de22c317ae050306356319e4acf670
msfnunesdev-commits <msfnunes.dev@gmail.com>
2026-05-17 22:57:06 -0300

    Move exercício conversor de dólar para cálculos básicos

--- dolar.py
+++
@@ -1,7 +0,0 @@
-print ("Seja bem vindo, ao Dolar Convert!")
-
-Real = float(input("Quantos reais você quer converter? R$ "))
-
-Dolar = Real/4.94
-
-print (f"Você terá, $ {round(Dolar, 2)} após a conversão.")
